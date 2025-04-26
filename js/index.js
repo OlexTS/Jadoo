699486bfe7f05f27f6233f1ee081c2bd292290aa
Olexandr <sashokseven@gmail.com>
2025-04-26 15:47:36 +0300

    start testimonials section

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.testimonials-section__swiper', {
+    // Optional parameters
+    
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      
+    },
+  
+   
+  });
