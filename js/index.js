24d7554ca5b33abbbf4fa51b3c5ac43aa929709e
Olexandr <sashokseven@gmail.com>
2025-04-29 23:28:27 +0300

    add swiper

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
-const swiper = new Swiper('.testimonials-section__swiper', {
-    // Optional parameters
-    
+
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.testimonials-section__swiper', {
     loop: true,
-  
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
-      
     },
-  
-   
   });
+});
