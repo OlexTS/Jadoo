0d6c74695db340646bfdd300c3d2fdf9ec638a7f
Olexandr <sashokseven@gmail.com>
2025-05-04 13:24:38 +0300

    add second slider

--- js/index.js
+++ js/index.js
@@ -3,8 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
   const swiper = new Swiper('.testimonials-section__swiper', {
     loop: true,
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-pagination--box1',
       clickable: true,
     },
   });
+  const swiper1 = new Swiper('.testimonials-section__box-2-swiper', {
+    direction: 'vertical',
+    loop: true,
+   
+    navigation: {
+      nextEl: '.swiper-button-next--box2',
+      prevEl: '.swiper-button-prev--box2',
+    },
+  });
 });
+
